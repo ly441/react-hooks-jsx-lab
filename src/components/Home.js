cbf26e3a7db81f8d57258b33f6c536b2fa94bbda
Lynn <koliilynn@gmail.com>
2025-04-17 22:10:02 +0300

    Update Home and NavBar components to enhance JSX structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { name, city } from "../data/data.js";
+const name ="lynn";
+const city ="new york"
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return(
+<div id="home">
+  <h1 style={{color:'firebrick'}}>{name} is a web developer from new york {city}</h1>
+
+</div>
+  );
 }
 
 export default Home;
